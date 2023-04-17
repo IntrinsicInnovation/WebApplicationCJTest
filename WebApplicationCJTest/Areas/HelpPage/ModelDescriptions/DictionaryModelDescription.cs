9e034c43a9c01f500e11787b6d8497dc7e0d6c07
Chris Johnson <chris.johnson@bcldb.ca>
2023-04-17 14:44:22 -0700

    Remove old app

--- WebApplicationCJTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApplicationCJTest.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebApplicationCJTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationCJTest.MvcApplication" Language="C#" %>
