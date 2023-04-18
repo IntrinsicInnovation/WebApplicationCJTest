23d24d9b3ef003955ac347bbb44300617323f901
Chris Johnson <chris.johnson@bcldb.ca>
2023-04-18 10:57:50 -0700

    Test layout change

--- CJWebApplication2/Startup.cs
+++ CJWebApplication2/Startup.cs
@@ -8,7 +8,7 @@ namespace CJWebApplication2
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); 
         }
     }
 }
